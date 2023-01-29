a9a578157463617418a7164250092948226fad46
Kim gitae <rlarlxo2628@gmail.com>
2023-01-29 22:14:21 +0900

    feat: movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { useEffect, useState } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoding] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => 
-    response.json()
-    ).then(json => {
-      setCoins(json);
-      setLoding(false);
-    });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}</li>)}
-      </ul>
-    </div>
-  );
+    return (
+    <Router>
+        <Routes>
+            <Route path="/movie/:id" element={<Detail />}>
+            </Route>
+            <Route path="/" element={<Home />}>
+            </Route>
+        </Routes>
+    </Router>
+    );
 }
 
 export default App;
